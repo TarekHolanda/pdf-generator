a22ca8d05c480a5eea3b6c4d12d1b78d75d93255
Tárek Holanda <12614592+TarekHolanda@users.noreply.github.com>
2025-07-15 10:45:12 -0700

    Update puppeteer chrome path

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ export async function generatePDF(data) {
     const html = renderInvoiceHTML(data);
 
     const browser = await puppeteer.launch({
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH ? process.env.PUPPETEER_EXECUTABLE_PATH : "/app/.apt/usr/bin/google-chrome",
         headless: "new",
         args: [
             "--no-sandbox",
